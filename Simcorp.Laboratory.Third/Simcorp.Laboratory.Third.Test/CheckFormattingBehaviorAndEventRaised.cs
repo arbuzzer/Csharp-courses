7e3d4d139f8afb8d77de77c579ec934c044860e7
Evhenii <zsteelgamerz@gmail.com>
2019-07-15 16:35:24 +0300

    add test

---
+++ Simcorp.Laboratory.Third/Simcorp.Laboratory.Third.Test/CheckFormattingBehaviorAndEventRaised.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Simcorp.Laboratory.Third.Test
+{
+    [TestClass]
+    public class CheckFormattingBehaviorAndEventRaised
+    {
+        [TestMethod]
+        public void CheckEventRaisedWhenMessageIsGenerated() {
+            SMSProvider SMSProvider = new SMSProvider();
+            var wait = new AutoResetEvent(false);
+            SMSProvider.SMSReceived += (message) => { wait.Set(); };
+            Assert.IsTrue(wait.WaitOne(TimeSpan.FromSeconds(1.5)));
+        }
+    }
+}
---
+++ Simcorp.Laboratory.Third/Simcorp.Laboratory.Third.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Simcorp.Laboratory.Third.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Simcorp.Laboratory.Third.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4ffdd378-107c-4733-8ac5-d1a449b6ea0d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
