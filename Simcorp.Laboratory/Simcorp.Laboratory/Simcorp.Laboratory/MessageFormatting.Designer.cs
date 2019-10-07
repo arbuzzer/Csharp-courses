eb509e5efd594afae3c12f20532494490212bd70
Evhenii <zsteelgamerz@gmail.com>
2019-10-08 00:34:16 +0300

    update designer

--- Simcorp.Laboratory/Simcorp.Laboratory/Simcorp.Laboratory/MessageFormatting.Designer.cs
+++ Simcorp.Laboratory/Simcorp.Laboratory/Simcorp.Laboratory/MessageFormatting.Designer.cs
@@ -26,6 +26,10 @@
         /// </summary>
         private void InitializeComponent() {
             this.MessageListView = new System.Windows.Forms.ListView();
+            this.DateTimeColumnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.NumberColumnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.NotificationColumnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.MessageTextColumnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.FiltrationCheckBox = new System.Windows.Forms.CheckBox();
             this.label1 = new System.Windows.Forms.Label();
             this.MessageFormatLabel = new System.Windows.Forms.Label();
@@ -41,20 +45,16 @@
             this.ChargingProgressBar = new System.Windows.Forms.ProgressBar();
             this.StartCharging = new System.Windows.Forms.Button();
             this.StopCharging = new System.Windows.Forms.Button();
-            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.FilterGroupBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // MessageListView
             // 
             this.MessageListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.columnHeader1,
-            this.columnHeader2,
-            this.columnHeader3,
-            this.columnHeader4});
+            this.DateTimeColumnHeader,
+            this.NumberColumnHeader,
+            this.NotificationColumnHeader,
+            this.MessageTextColumnHeader});
             this.MessageListView.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.MessageListView.HideSelection = false;
             this.MessageListView.Location = new System.Drawing.Point(12, 338);
@@ -274,10 +274,10 @@
         private System.Windows.Forms.ProgressBar ChargingProgressBar;
         private System.Windows.Forms.Button StartCharging;
         private System.Windows.Forms.Button StopCharging;
-        private System.Windows.Forms.ColumnHeader columnHeader1;
-        private System.Windows.Forms.ColumnHeader columnHeader2;
-        private System.Windows.Forms.ColumnHeader columnHeader3;
-        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader DateTimeColumnHeader;
+        private System.Windows.Forms.ColumnHeader NumberColumnHeader;
+        private System.Windows.Forms.ColumnHeader NotificationColumnHeader;
+        private System.Windows.Forms.ColumnHeader MessageTextColumnHeader;
     }
 }
 
